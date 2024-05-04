ef464a287c84eed80bc524517162db9ba9d0c22c
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-05-04 22:34:21 +0200

    Initial Commit

---
+++ SmartImageLoader/SmartImageLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartImageLoaderApp.swift
+//  SmartImageLoader
+//
+//  Created by Pavankumar Arepu on 04/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SmartImageLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
