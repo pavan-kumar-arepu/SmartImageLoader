3bc71ec4b13c14689e7a890b715cbea551a3c4a3
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-05-05 21:06:24 +0200

    Addition: Implemented ThumbnailDetailView and passed the required to show the Thumbnail details

--- MediaCoverage/Views/MediaCoverageView.swift
+++ MediaCoverage/Views/MediaCoverageView.swift
@@ -12,6 +12,42 @@ struct MediaCoverageView: View {
     var mediaCoverage: MediaCoverage
     
     @State private var image: UIImage?
+
+    var body: some View {
+        NavigationLink(destination: ThumbnailDetailView(mediaCoverage: mediaCoverage)) {
+            VStack(alignment: .leading, spacing: 8) {
+                if let image = image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 100, height: 100)
+                        .cornerRadius(8)
+                        .padding(.bottom, 4)
+                } else {
+                    ProgressView()
+                        .frame(width: 100, height: 100)
+                        .padding(.bottom, 4)
+                }
+            }
+            .padding(8)
+            .background(Color.gray.opacity(0.1))
+            .cornerRadius(8)
+        }
+        .onAppear {
+            viewModel.loadImage(for: mediaCoverage) { loadedImage in
+                self.image = loadedImage
+            }
+        }
+    }
+}
+
+
+/*
+struct MediaCoverageView: View {
+    @ObservedObject var viewModel: MediaCoverageViewModel
+    var mediaCoverage: MediaCoverage
+    
+    @State private var image: UIImage?
     
     var body: some View {
         VStack(alignment: .leading, spacing: 8) {
@@ -38,6 +74,7 @@ struct MediaCoverageView: View {
         }
     }
 }
+ */
 
 
 //struct MediaCoverageView_Previews: PreviewProvider {
