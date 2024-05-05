434bb4055568e4eac1d6442ca261ae58ff49df41
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-05-05 15:52:38 +0200

    Removed unnecessary folders

--- MediaCoverage/DataLayer/MediaCoverageListView.swift
+++ MediaCoverage/DataLayer/MediaCoverageListView.swift
@@ -11,17 +11,34 @@ import SwiftUI
 struct MediaCoverageListView: View {
     @ObservedObject var viewModel: MediaCoverageViewModel
     
+    @State private var isNavigationBarHidden = true
+    
     var body: some View {
         ScrollView {
-            LazyVGrid(columns: Array(repeating: GridItem(), count: 3), spacing: 10) {
-                ForEach(viewModel.mediaCoverages) { mediaCoverage in
-                    MediaCoverageView(viewModel: viewModel, mediaCoverage: mediaCoverage)
+            VStack {
+                // Header Image (AcharyaPrasanth)
+                GeometryReader { geometry in
+                    Image("acharya_prashant.png")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(height: 200) // Adjust height as needed
+                        .clipped()
+                        .offset(y: -geometry.frame(in: .global).minY)
+                        .onAppear {
+                            self.isNavigationBarHidden = geometry.frame(in: .global).minY > 0
+                        }
                 }
-            }
-            .padding()
-            .onAppear {
-                viewModel.fetchMediaCoverages()
+                
+                // Grid of Media Coverages
+                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())], spacing: 8) {
+                    ForEach(viewModel.mediaCoverages) { mediaCoverage in
+                        MediaCoverageView(viewModel: viewModel, mediaCoverage: mediaCoverage)
+                    }
+                }
+                .padding()
             }
         }
+        .navigationBarTitle("Acharya Prasanth", displayMode: .inline)
+        .navigationBarHidden(isNavigationBarHidden)
     }
 }
